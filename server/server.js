ec7c2acb8539719ad7e93ec8520c7606ff33b46c
James Warren Basham <James Warren Basham>
2017-01-30 15:02:59 -0800

    Renewed Interface and Firmware and Middleware for the server

--- server/server.js
+++ server/server.js
@@ -6,82 +6,29 @@ const socketIO = require('socket.io');
 const axios = require('axios');
 const moment = require('moment');
 const mustache = require('mustache');
+const jquery = require('jquery');
+const njs = require('njs');
+const infrajs = require('infrajs');
+const HTMLpaser = require('htmlparser');
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '/weather');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connect', function () {
-  console.log('Connected to server');
+const publicPath = (express.static(__dirname + '/weather'));
+app.set('views', publicPath);
+app.engine('html', require('ejs').renderfile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('home.html');
 });
-
-io.on('disconnect', function () {
-  console.log('Disconnected from server');
-});
-
-// jQuery('#address-form').on('submit', function (e) {
-//   e.preventDefault();
-//
-// // socket.emit('createAddress', {
-// //     from: 'User',
-// //     text: jQuery('[userAddress=address]').val()
-// //   }, function () {
-// //
-// //   });
-// // });
-//
-// var locationButton = jQuery('#get-weather');
-// var encodedAddress = encodeURIComponent(locationButton);
-// var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
-//
-// locationButton.on('click', function () {
-//   axios.get(geocodeUrl).then((response) => {
-//   if (response.data.status === 'ZERO_RESULTS') {
-//     throw new Error('Unable to find that address.');
-//   }
-//
-//   var lat = response.data.results[0].geometry.location.lat;
-//   var lng = response.data.results[0].geometry.location.lng;
-//
-//   var location = response.data.results[0].formatted_address;
-//
-//   var weatherUrl = `https://api.forecast.io/forecast/518478e31f58ffdcfef37f1c12700881/${lat},${lng}`;
-//   axios.get(weatherUrl);
-//   var tmp = response.data.currently.temperature;
-//   var apprTmp = response.data.currently.apparentTemperature;
-//
-//   })
-// }).then((response) => {
-//   app.get('/', (req, res) => {
-//     res.render('./weather/home.html', {
-//       formatAddy: location,
-//       realTemp: tmp,
-//       apprTemp: apprTmp
-//     });
-//   });
-// }).catch((e) => {
-//   if (e.code === 'ENOTFOUND') {
-//     alert('Unable to connect to API servers.');
-//   } else {
-//     alert(e.address);
-//   }
-// });
-
-  // socket.emit('addResults', {
-  //   formatAddy: location,
-  //   realTemp: tmp,
-  //   apprTemp: apprTmp
-  // });
-//
-// app.get('/', (req, res) => {
-//   res.render('./weather/home.html', {
-//     formatAddy: location,
-//     realTemp: temperature,
-//     apprTemp: apparentTemperature
-//   });
-// });
 
 server.listen(port, function () {
   console.log(`Server is up on port ${port}`);
--- weather/js/index.js
+++ weather/js/index.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
+const jquery = require('jquery');
+const express = require('express');
+const moment = require('moment');
+
 var socket = io();
+
 
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -9,47 +14,54 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-var location, lat, lng, formatLocation, temp, apprTemp, timeOfReqest;
+var lat, lng, formatLocation, temp, apprTemp, timeOfReqest;
 
-jQuery('#address-form').on('submit', function (e) {
+jquery('#addressForm').on('submit', function (e) {
   e.preventDefault();
-
-  socket.emit('createaddress', {
-    text: jQuery('[userAddress=address]').val()
-  }, function (result) {
-    location = text;
-});
+  var location = jquery('#text').val();
   var encodedAddress = encodeURIComponent(location);
   var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
-  locationButton.on('click', function () {
+  var weatherUrl = `https://api.forecast.io/forecast/518478e31f58ffdcfef37f1c12700881/${lat},${lng}`;
+
+  //Call API to get coordinates for given address, if available.
     axios.get(geocodeUrl).then((response) => {
       if (response.data.status === 'ZERO_RESULTS') {
         throw new Error('Unable to find that address.');
       }
+
       lat = response.data.results[0].geometry.location.lat;
       lng = response.data.results[0].geometry.location.lng;
 
       formatLocation = response.data.results[0].formatted_address;
+
       timeOfReqest = moment.format(MMMM Do YYYY, h:mm:ss);
-      var weatherUrl = `https://api.forecast.io/forecast/518478e31f58ffdcfef37f1c12700881/${lat},${lng}`;
-      axios.get(weatherUrl);
-    }).then((response) =>
+  //With Longitude and Latitude, call API to get weather information.
+      axios.get(weatherUrl).then((response) => {
 
-    temp = response.data.currently.temperature;
-    apprTemp = response.data.currently.apparentTemperature;
+      temp = response.data.currently.temperature;
+      apprTem = response.data.currently.apparentTemperature;
 
-  }).catch((e) => {
-    if (e.code === 'ENOTFOUND') {
-      alert('Unable to connect to API servers.');
-    } else {
-      alert(e.address);
-    }
+      }).catch((e) => {
+      if (e.code === 'ENOTFOUND') {
+        alert('Unable to connect to API servers.');
+      } else {
+        alert(e.address);
+      }
+    )}
+  )};
+)};
 
-    socket.emit('addResults', {
-      timeStamp: timeOfReqest,
-      formatAddy: formatLocation,
-      realTemp: temp,
-      apprTemp: apprTemp
-    });
+socket.on('addressMessage' function (weather) {
+  if (jquery('#weather').isNotEmpty() => jquery('#weather').empty());
+  var li = jquery('<li></li>');
+  li.text(`At: ${timestamp}: \n
+           On or Around: ${formatLocation} \n
+           It was ${temp} outside, yet \n
+           it may have felt like it was ${apprTemp}.`);
+  jquery('#weather').append(li);
+  socket.emit('emitWeather', {
+    res('/').render('./weather/home.html', {
+    weather: jquery('#weather')
+  )};
   });
 });
