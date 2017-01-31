358725185f34be65e6101269b95169f2bec4b471
James Warren Basham <James Warren Basham>
2017-01-31 13:04:36 -0800

    Interface with AJAX code built in

--- weather/js/index.js
+++ weather/js/index.js
@@ -6,62 +6,112 @@ const moment = require('moment');
 var socket = io();
 
 
-socket.on('connect', function () {
-  console.log('Connected to server');
-});
+// socket.on('connect', function () {
+//   console.log('Connected to server');
+// });
 
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
-});
+// socket.on('disconnect', function () {
+//   console.log('Disconnected from server');
+// });
 
-var lat, lng, formatLocation, temp, apprTemp, timeOfReqest;
+var location, lat, lng, formatLocation, temp, apprTemp, timeOfReqest;
 
-jquery('#addressForm').on('submit', function (e) {
-  e.preventDefault();
-  var location = jquery('#text').val();
+jquery('#submit').click(function(event) {
+  event.preventDefault();
+
+  $.ajax(this.href, {
+    success: function(data) {
+      $('#addressForm').html($(data).find('#text *'));
+      location = data;
+    },
+    error: function() {
+      alert('No address has been entered');
+    }
+  });
+
   var encodedAddress = encodeURIComponent(location);
-  var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
-  var weatherUrl = `https://api.forecast.io/forecast/518478e31f58ffdcfef37f1c12700881/${lat},${lng}`;
-
-  //Call API to get coordinates for given address, if available.
-    axios.get(geocodeUrl).then((response) => {
-      if (response.data.status === 'ZERO_RESULTS') {
-        throw new Error('Unable to find that address.');
-      }
-
+  var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+  var timeStamp = moment.format(MMMM Do YYYY, h:mm:ss);
+  $.ajax({
+    url: geocodeUrl,
+    data: {
+        format: 'json'
+    },
+    error: function() {
+      alert('An Error Has Occurred');
+    },
+    dataType: 'jsonp',
+    success: function (data) {
       lat = response.data.results[0].geometry.location.lat;
       lng = response.data.results[0].geometry.location.lng;
+      var $time = $('<p>').text(timeStamp);
+      var $place = $('<p>').text(response.data.results[0].formatted_address);
+      $('#weather')
+        .append($time)
+        .append($place);
+    },
+    type: 'GET'
 
-      formatLocation = response.data.results[0].formatted_address;
+    location.reload(true);
+  });
 
-      timeOfReqest = moment.format(MMMM Do YYYY, h:mm:ss);
-  //With Longitude and Latitude, call API to get weather information.
-      axios.get(weatherUrl).then((response) => {
+  var weatherUrl = `https://api.forecast.io/forecast/518478e31f58ffdcfef37f1c12700881/${lat},${lng}`;
 
-      temp = response.data.currently.temperature;
-      apprTem = response.data.currently.apparentTemperature;
+  $.ajax({
+    url: weatherUrl,
+    data: {
+      format: 'json'
+    },
+    error: function() {
+      alert('An Error Has Occurred Getting the Weather');
+    },
+    dataType: 'jsonp',
+    success: function(data) {
+      var $temp = $('<p>It is ').text(response.data.currently.temperature);
+      var $appTemp = $('<p>It feels like ').text(response.data.currently.apparentTemperature);
+      $('#weather')
+        .append($temp),
+        .append($appTemp);
+    },
+    type: 'GET'
+  });
 
-      }).catch((e) => {
-      if (e.code === 'ENOTFOUND') {
-        alert('Unable to connect to API servers.');
-      } else {
-        alert(e.address);
-      }
-    )}
-  )};
+  $('#weather').load(this.href + '#weather *')};
 )};
 
-socket.on('addressMessage' function (weather) {
-  if (jquery('#weather').isNotEmpty() => jquery('#weather').empty());
-  var li = jquery('<li></li>');
-  li.text(`At: ${timestamp}: \n
-           On or Around: ${formatLocation} \n
-           It was ${temp} outside, yet \n
-           it may have felt like it was ${apprTemp}.`);
-  jquery('#weather').append(li);
-  socket.emit('emitWeather', {
-    res('/').render('./weather/home.html', {
-    weather: jquery('#weather')
-  )};
-  });
-});
+
+  //Call API to get coordinates for given address, if available.
+  //   axios.get(geocodeUrl).then((response) => {
+  //     if (response.data.status === 'ZERO_RESULTS') {
+  //       throw new Error('Unable to find that address.');
+  //     }
+  //
+  //     lat = response.data.results[0].geometry.location.lat;
+  //     lng = response.data.results[0].geometry.location.lng;
+  //
+  //     formatLocation = response.data.results[0].formatted_address;
+  //
+  //     timeOfReqest = moment.format(MMMM Do YYYY, h:mm:ss);
+  // //With Longitude and Latitude, call API to get weather information.
+  //     axios.get(weatherUrl).then((response) => {
+  //
+  //
+  //     apprTem = response.data.currently.apparentTemperature;
+  //
+  //     }).catch((e) => {
+  //     if (e.code === 'ENOTFOUND') {
+  //       alert('Unable to connect to API servers.');
+  //     } else {
+  //       alert(e.address);
+//       }
+//     )}
+//   )};
+// )};
+
+// socket.on('addressMessage' function (weather) {
+//   if (jquery('#weather').isNotEmpty() => jquery('#weather').empty());
+//   socket.emit('emitWeather', {
+//     res('/').render('./weather/home.html');
+//   )};
+//   });
+// });
